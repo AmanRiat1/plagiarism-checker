974fa1fc8225f0877ce555db4c6d06c72c09a109
Aman Riat <amanriat777@gmail.com>
2019-03-30 00:41:05 -0400

    Finished LinkedWordMap

--- LinkedWordMap.java
+++ LinkedWordMap.java
@@ -6,6 +6,30 @@
 
 public class LinkedWordMap implements WordMap {
 
+    private static class Node{
+        
+        private String key;
+        private int count;
+        private Node next;
+        private Node previous;
+
+        private Node (String key, Node previous, Node next){
+            this.key = key;
+            this.next = next;
+            this.previous = previous;
+            count++;
+        }
+    }
+
+    private Node head;
+
+    public LinkedWordMap(){
+        head = new Node(null,null,null);
+        head.next = head;
+        head.previous = head; 
+    }
+   
+
     /**
      * Returns the logical size of this WordMap. That is the number of
      * associations currently stored in it.
@@ -14,8 +38,13 @@ public class LinkedWordMap implements WordMap {
      */
     
     public int size() {
-
-        throw new UnsupportedOperationException("not implemented yet!");
+        int size = 0;
+        Node current = head;
+        while (current.next != head){
+            current = current.next;
+            size ++;
+        }
+        return size; 
         
     }
 
@@ -30,7 +59,19 @@ public class LinkedWordMap implements WordMap {
 
     public boolean contains(String key) {
 
-        throw new UnsupportedOperationException("not implemented yet!");
+        if (key == null){
+            throw new NullPointerException("Not a valid key");
+        }
+ 
+        //Using while loop here instead of using size to reduce time, calling size will result in 2 loops or n*n time 
+        Node current = head;
+        while (current.next != head){
+            current = current.next;
+            if (key.equals(current.key)){
+                return true; 
+            }
+        }
+        return false;
         
     }
     
@@ -45,8 +86,18 @@ public class LinkedWordMap implements WordMap {
     
     public int get(String key) {
 
-        throw new UnsupportedOperationException("not implemented yet!");
-        
+        if (key == null){
+            throw new NullPointerException();
+        }   
+
+        Node current = head;
+        while(current.next != head){
+            current = current.next;
+            if (key.equals(current.key)){
+                return current.count;
+            }
+        }
+        return 0;     
     }
     
     /**
@@ -59,8 +110,47 @@ public class LinkedWordMap implements WordMap {
      */
 
     public void update(String key) {
+        if (key == null){
+            throw new NullPointerException();
+        }
 
-        throw new UnsupportedOperationException("not implemented yet!");        
+        Node current = head;
+        while(current.next != head){
+            current = current.next;
+            if (key.equals(current.key)){
+                current.count += 1;
+                return;
+            }
+            
+        }
+        if (head.next == head){
+          Node p = new Node(key,head,head);
+          head.next = p;
+          head.previous =p;
+          return;
+        }
+
+        current = head;
+        while (current.next != head){
+            current = current.next;
+            if (current.key.compareTo(key)>= 1 ){
+                Node p = new Node(key, current.previous, current);
+                current.previous.next = p;
+                current.previous = p;
+                return;
+            }
+            if (current.next == head){
+                Node p = new Node(key,current, head);
+                current.next = p;
+                head.previous = p;
+                return;
+          }
+        }
+        //if alphabetical will have to change this 
+        Node newElem = new Node(key,current,head);
+        //heads current previous points to last element and we connect that last element to new Node 
+        current.next = newElem;
+        head.previous = newElem;    
         
     }
     
@@ -72,8 +162,13 @@ public class LinkedWordMap implements WordMap {
      */
     
     public String[] keys() {
-        
-        throw new UnsupportedOperationException("not implemented yet!");
+        Node current = head;
+        String[] keys = new String[size()];
+        for (int i = 0; i < size(); i++){
+            current = current.next;
+            keys[i] = current.key;
+        }
+        return keys;
         
     }
 
@@ -87,9 +182,14 @@ public class LinkedWordMap implements WordMap {
     
     public Integer[] counts() {
         
-        throw new UnsupportedOperationException("not implemented yet!");
+        Node current = head;
+        Integer[] count = new Integer[size()];
+        for (int i = 0; i < size(); i++){
+            current = current.next;
+            count[i] = new Integer(current.count);
+        }
+        return count;
         
     }
-
     
 }
